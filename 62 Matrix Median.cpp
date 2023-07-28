064b669a81c038dfc4a7d74e0f57ed604ae2fb25
Alok Singh <98412930+inquisitive9@users.noreply.github.com>
2023-07-28 21:22:02 +0530

    Update 62 Matrix Median.cpp

--- 62 Matrix Median.cpp
+++ 62 Matrix Median.cpp
@@ -1,1 +1,95 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+ 
+
+int getMedian(vector<vector<int>>& A) {
+
+    int row = A.size();
+
+    int col = A[0].size();
+
+ 
+
+    bool even;
+
+    if ((row * col) % 2 == 0)
+
+        even = true;
+
+    else
+
+        even = false;
+
+ 
+
+    int size = (row * col) / 2 + 1;
+
+ 
+
+    vector<int> ans;
+
+ 
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    vector<int> idx(row, 0);
+
+ 
+
+    for (int i = 0; i < row; i++) {
+
+        pq.push({A[i][idx[i]], i});
+
+        idx[i]++;
+
+    }
+
+ 
+
+    while (size--) {
+
+        int val = pq.top().first;
+
+        int i = pq.top().second;
+
+        pq.pop();
+
+        ans.push_back(val);
+
+ 
+
+        if (idx[i] == col)
+
+            continue;
+
+ 
+
+        pq.push({A[i][idx[i]], i});
+
+        idx[i]++;
+
+    }
+
+ 
+
+    int res = 0;
+
+ 
+
+    if (even)
+
+        res = (ans.back() + ans[ans.size() - 2]) / 2; // Corrected index for median calculation
+
+    else
+
+        res = ans.back();
+
+ 
+
+    return res;
+
+}
+
+ 
