56163024c10ef59c80f5bbd520e6b2f8949b1e60
Alok Singh <98412930+inquisitive9@users.noreply.github.com>
2023-07-28 20:45:37 +0530

    Create 44 Minimum Number of Platforms.cpp

---
+++ 44 Minimum Number of Platforms.cpp
@@ -0,0 +1,34 @@
+int calculateMinPatforms(int at[], int dt[], int n) {
+    // Write your code here.
+    sort(at,at+n);
+
+    sort(dt,dt+n);
+
+    int i=0,j=0,ans=0,res=0;
+
+    while(i<n and j<n){
+
+        if(at[i]<=dt[j]){
+
+            ans++;
+
+            i++;
+
+        }
+
+        else{
+
+            ans--;
+
+            j++;
+
+        }
+
+        res=max(res,ans);
+
+    }
+
+    return res;
+
+ 
+}
