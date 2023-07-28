74a4f1d09f5793c019c074acb3c1b0c376063b79
Alok Singh <98412930+inquisitive9@users.noreply.github.com>
2023-07-28 22:31:41 +0530

    Create  151. Convert A Given Binary Tree To Doubly Linked List.cpp

---
+++ 151. Convert A Given Binary Tree To Doubly Linked List.cpp
@@ -0,0 +1,16 @@
+void help(BinaryTreeNode<int>*root,BinaryTreeNode<int>*&prev,BinaryTreeNode<int>*&head){
+    if(root==NULL)return;
+    help(root->left,prev,head);
+    if(head==NULL)head=root;
+    root->left=prev;
+    if(prev!=NULL)prev->right=root;
+    prev=root;
+    help(root->right,prev,head);   
+}
+BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
+    // Write your code here
+    BinaryTreeNode<int>* p=NULL;
+    BinaryTreeNode<int>* head=NULL;
+    help(root,p,head);
+    return head;
+}
