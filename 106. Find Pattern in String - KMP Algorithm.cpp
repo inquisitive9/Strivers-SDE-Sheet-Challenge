fe4a7029e95879dad1ad937aa866ace0b6469f38
Alok Singh <98412930+inquisitive9@users.noreply.github.com>
2023-07-29 21:33:56 +0530

    Create  106. Find Pattern in String - KMP Algorithm.cpp

---
+++ 106. Find Pattern in String - KMP Algorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+
+bool findPattern(string p, string s)
+
+{
+
+    // Write your code here.
+
+    int ssize=s.size(),psize=p.size();
+
+    for(int i=0;i<ssize-psize+1;i++){
+
+        if(s.substr(i,psize)==p){
+
+            return true;
+
+        }
+
+    }
+
+    return false;
+
+}
