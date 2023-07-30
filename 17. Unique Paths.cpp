b5b6cd45dd73221ae289892653ae31d063e31d94
Alok Singh <98412930+inquisitive9@users.noreply.github.com>
2023-07-30 21:14:40 +0530

    Create  17. Unique Paths.cpp

---
+++ 17. Unique Paths.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+  int helper(int i,int j,vector<vector<int>>& dp) {
+        if(i == 0 && j ==0) {
+            return 1;
+        }
+        if(i<0 || j <0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int l = helper(i-1,j,dp);
+        int r = helper(i,j-1,dp);
+        return dp[i][j] = l + r;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        if(m==1 || n==1 ) {
+            return 1;
+        }
+        int N = m+n-2;
+        int r = m-1;
+        double ans = 1.0;
+        for(int i = 1; i <=(m-1) ;i++) {
+            ans=((ans)*(N-r+i))/i ;
+        }
+        return (int)ans;
+    }
